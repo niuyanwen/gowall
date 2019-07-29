50213928f1be9bddc09dcf468d32e842db876783
niuyanwen <975279981@qq.com>
2019-07-29 18:02:37 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// jsproxy_config=x=>{__CONF__=x;importScripts(__FILE__=x.assets_cdn+'bundle.6fecf114.js')};importScripts('conf.js')
-jsproxy_config=x=>{__CONF__=x;importScripts(__FILE__=x.assets_cdn+'bundle.c33e24c5.js')};importScripts('conf.js')
+jsproxy_config=x=>{__CONF__=x;importScripts(__FILE__=x.assets_cdn+'bundle.6fecf114.js')};importScripts('conf.js')
+// jsproxy_config=x=>{__CONF__=x;importScripts(__FILE__=x.assets_cdn+'bundle.c33e24c5.js')};importScripts('conf.js')
