782dc8c50ec7251fcd372400fe9d02c134550126
niuyanwen <975279981@qq.com>
2019-10-12 10:41:25 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -56,7 +56,7 @@ jsproxy_config({
    * 默认节点
    */
   // node_default: 'mysite',
-  node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
+  node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'demo-sg',
 
   /**
    * 加速节点
