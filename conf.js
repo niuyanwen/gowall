0b7d012a735e7c243c1595b9559a5e0edb210fe2
niuyanwen <975279981@qq.com>
2020-09-07 09:53:59 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -3,7 +3,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '110',
+  ver: '107',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -28,25 +28,10 @@ jsproxy_config({
         'node-aliyun-sg.etherdream.com:8443': 1,
       },
     },
-    'demo-niu': {
-      label: '演示服务-牛牛牛（有频率限制）',
-      lines: {
-        // 收费版（高权重）
-        'node-cfworker.etherdream.com': 4,
-
-        // 免费版（低权重，分摊一些成本）
-        // 每个账号每天 10 万次免费请求，但有频率限制
-        'b.007.workers.dev': 1,
-        'b.hehe.workers.dev': 1,
-        'b.lulu.workers.dev': 1,
-        'b.jsproxy.workers.dev': 1,
-      },
-    },
     'mysite': {
-      label: '当前站点11',
+      label: '当前站点',
       lines: {
-       // [location.host]: 1,
-         'node-aliyun-sg.etherdream.com:8443': 1,
+        [location.host]: 1,
       }
     },
     // 该节点用于加载大体积的静态资源
